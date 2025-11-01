a87ed7715561d11e09b0070910c231e2f1123a6f
DESKTOP-9544MJ9\NoxiePC <ricardoahmar@gmail.com>
2025-11-01 17:52:48 -0300

    proyecto

---
+++ src/main/java/com/project/loginVaS/LoginVaSApplication.java
@@ -0,0 +1,13 @@
+package com.project.loginVaS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginVaSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginVaSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/loginVaS/LoginVaSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.loginVaS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginVaSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
